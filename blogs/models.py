1c4123e3cc5abc2548dbbf6af8e9bc573deee8a5
sonirakhan <sonira16khan@gmail.com>
2025-10-29 12:07:48 +0530

    changes in model Comment

--- blogs/models.py
+++ blogs/models.py
@@ -17,26 +17,6 @@ class Post(models.Model):
     status = models.CharField(max_length=30,choices=STATUS_CHOICES,default='DRAFT')
 
 
-class Comment(models.Model):
-    comment_post = models.ForeignKey(Post,on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=50)
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ['created']
-
-    def __str__(self):
-        return f'Comment by {self.name}'
-
-
-
-
-
-
     class Meta:
         ordering = ['-created']
 
@@ -51,4 +31,17 @@ class Comment(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f'Comment by {self.name}'
